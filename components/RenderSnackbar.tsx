5c42bdc13362b4d5b74f7ef4eaa22c903207b438
kishorekiu <kishorekrissh7@gmail.com>
2026-01-17 16:09:46 +0530

    optimised snackbar to popup only when user login/logout, added a ref isMounted and skipping snackbar open on initial mount/page load/refresh

--- components/RenderSnackbar.tsx
+++ components/RenderSnackbar.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { useAppSelector } from "@/lib/hooks";
 import { Snackbar } from "@mui/material";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 const RenderSnackbar = () => {
   const isAuth = useAppSelector((state) => state.auth.isAuth);
   const [open, setOpen] = React.useState(false);
+  const isMounted = useRef(false);
   useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      return;
+    }
     setOpen(true);
   }, [isAuth]);
   return (
